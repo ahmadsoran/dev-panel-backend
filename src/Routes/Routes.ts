57d2ec06104875b835344a62ca18a065e65f69d7
ahmadsoran <ahmadsorann@gmail.com>
2023-01-23 15:41:28 +0300

    new routes

--- src/Routes/Routes.ts
+++ src/Routes/Routes.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import AuthRoute from "./Auth/SignIn";
+import AuthRoute from "./Auth/Auth";
+import DevPanelRoutes from "./dev-panel/DevPanelRoutes";
+import PlatformRoutes from "./Platform/PlatformRoutes";
 
 const Routes = Router();
 
 Routes.use("/auth", AuthRoute);
+Routes.use("/dev-panel", DevPanelRoutes);
+Routes.use("/platform", PlatformRoutes);
 
 export default Routes;
