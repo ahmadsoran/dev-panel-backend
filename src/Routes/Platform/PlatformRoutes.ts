ba5400ad19a1f34dc6bda57f8e254ee0fa052490
ahmadsoran <ahmadsorann@gmail.com>
2023-01-24 00:16:22 +0300

    change some routes

--- src/Routes/Platform/PlatformRoutes.ts
+++ src/Routes/Platform/PlatformRoutes.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import AddPlatform from "../../controller/Platform/AddPlatform";
+import AddUser from "../../controller/Platform/AddUser";
 import GenApiKeyForPlatforms from "../../controller/Platform/Gen-apikey";
-import { isAdmin, isSuperAdmin } from "../../middlewears/Auth";
+import { isAdmin, isApiKeyValid, isSuperAdmin } from "../../middlewears/Auth";
 
 const PlatformRoutes = Router();
 // full path /api/API_VERSION/platform/*
 
 PlatformRoutes.post("/add", isAdmin, AddPlatform);
 PlatformRoutes.post("/gen-apikey", isSuperAdmin, GenApiKeyForPlatforms);
+PlatformRoutes.post("/user/add", isApiKeyValid, AddUser);
 
 export default PlatformRoutes;
