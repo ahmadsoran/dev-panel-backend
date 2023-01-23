0168cbf109d48216a18d3a7fb22cc96d5183105e
ahmadsoran <ahmadsorann@gmail.com>
2023-01-23 15:34:59 +0300

    add winston logger

--- src/App.ts
+++ src/App.ts
@@ -1,26 +1,28 @@
 import express, { Express } from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import { ConnectDB } from "./db/connection/connection";
+import ConnectDB from "./db/connection/connection";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import Routes from "./Routes/Routes";
 import Api from "./util/Api";
+import WinstonLogger from "./db/logger/WinstonLogger";
 dotenv.config();
 const app: Express = express();
 const ENV = process.env;
 
-app.use(helmet());
-app.use(cors());
-app.use(cookieParser());
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/data", express.static(`${__dirname}/public`));
+app.use(express.json());
 
 StartServer();
 async function StartServer() {
   ConnectDB();
+  app.use(helmet());
+  app.use(cors());
+  app.use(cookieParser());
   app.use(`${Api.RootRoute}/${Api.Version}`, Routes);
+  app.use("/data", express.static(`${__dirname}/public`));
+  WinstonLogger("error", "errors");
   app.listen(ENV.PORT, () => {
     console.log(`app listing on ${ENV.PORT}`);
   });
