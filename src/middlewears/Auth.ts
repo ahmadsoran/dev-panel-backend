a5fcffa4ae09f0549c0d0f9db83b43dcbd6138ce
ahmadsoran <ahmadsorann@gmail.com>
2023-01-23 15:41:19 +0300

    fix undifined token

--- src/middlewears/Auth.ts
+++ src/middlewears/Auth.ts
@@ -1,34 +1,38 @@
 import { NextFunction, Request, Response } from "express";
 import winston from "winston";
 import jwt from "jsonwebtoken";
-import AdminAccountSchema from "../db/model/AdminAccountSchema";
-import PlatformsApiKeySchema from "../db/model/PlatformApikeySchema";
+import AdminAccountSchema from "../db/model/Dev-PanelSchema/AdminAccountSchema";
+import PlatformsApiKeySchema from "../db/model/PlatformSchema/Apikey";
 type decodedToken = {
-  _id: string;
+  id: string;
 };
 export async function isAdmin(req: Request, res: Response, Next: NextFunction) {
   const token: string = req.cookies?.token || "";
-  const privateKey = process.env.PRV_TOKEN;
+  const privateKey = process.env.PRV_KEY;
   try {
     if (!privateKey) {
       winston.error("private key is undifined");
       return res.status(500).send("unkown error happen try again later");
     }
-
+    if (!token)
+      return res.status(401).send("un authorized please sign in first");
     const VerifiedToken: decodedToken = jwt.verify(
       token,
       privateKey
     ) as decodedToken;
 
-    const isAdminUser = await AdminAccountSchema.findById(VerifiedToken?._id);
-
-    if (!VerifiedToken?._id)
+    const AdminUser = await AdminAccountSchema.findById(VerifiedToken?.id);
+    if (!VerifiedToken?.id)
       return res.status(401).send("you are not authorized to do this action");
-    if (isAdminUser?.role === "admin" || isAdminUser?.role === "superadmin")
+
+    if (AdminUser?.role === "admin" || AdminUser?.role === "superadmin") {
+      req.AdminID = AdminUser?._id;
       return Next();
+    }
     return res.status(401).send("you are not authorized to do this action");
   } catch (error) {
     if (error instanceof Error) {
+      winston.error(`error while authenticate admin msg:${error.message}`);
       return res.status(500).send("unkown error happen please try again later");
     }
   }
@@ -39,27 +43,35 @@ export async function isSuperAdmin(
   res: Response,
   Next: NextFunction
 ) {
-  const token: string = req.cookies?.token || "";
-  const privateKey = process.env.PRV_TOKEN;
+  const token = req.cookies.token;
+  const privateKey = process.env.PRV_KEY;
+
   try {
     if (!privateKey) {
       winston.error("private key is undifined");
       return res.status(500).send("unkown error happen try again later");
     }
+    if (!token)
+      return res.status(401).send("un authorized please sign in first");
 
     const VerifiedToken: decodedToken = jwt.verify(
       token,
       privateKey
     ) as decodedToken;
-
-    const isAdminUser = await AdminAccountSchema.findById(VerifiedToken?._id);
-
-    if (!VerifiedToken?._id)
+    if (!VerifiedToken?.id)
       return res.status(401).send("you are not authorized to do this action");
-    if (isAdminUser?.role === "superadmin") return Next();
+
+    const AdminUser = await AdminAccountSchema.findById(VerifiedToken?.id);
+    if (AdminUser?.role === "superadmin") {
+      req.AdminID = AdminUser?._id;
+      return Next();
+    }
     return res.status(401).send("you are not authorized to do this action");
   } catch (error) {
     if (error instanceof Error) {
+      winston.error(
+        `error while authenticate super admin msg:${error.message}`
+      );
       return res.status(500).send("unkown error happen please try again later");
     }
   }
@@ -75,7 +87,7 @@ export async function isApiKeyValid(
   Next: NextFunction
 ) {
   const ApiKey = req.headers.authorization;
-  const privateKey = process.env.PRV_TOKEN;
+  const privateKey = process.env.PRV_KEY;
   if (!ApiKey) return res.status(400).send("provide api key");
   if (!privateKey)
     return res.status(500).send("something went wrong! try again later");
@@ -86,6 +98,7 @@ export async function isApiKeyValid(
     });
     if (!ValidateApiKey)
       return res.status(401).send("unauthorized to do this action");
+
     Next();
   } catch (error) {
     if (error instanceof Error) {
