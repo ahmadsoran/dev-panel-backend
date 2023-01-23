bda8ffb3e7daee15211f22f052132a5a0b183c19
ahmadsoran <ahmadsorann@gmail.com>
2023-01-23 15:35:43 +0300

    fix signin issue

--- src/controller/Auth/Signin.ts
+++ src/controller/Auth/Signin.ts
@@ -1,9 +1,10 @@
 import { Request, Response } from "express";
 import SignInDataValidation from "../../Validations/SignIn";
 import bcrypt from "bcrypt";
-import AdminAccountSchema from "../../db/model/AdminAccountSchema";
+import AdminAccountSchema from "../../db/model/Dev-PanelSchema/AdminAccountSchema";
 import jwt from "jsonwebtoken";
 import dayjs from "dayjs";
+import winston from "winston";
 
 export default async function SiginIn(req: Request, res: Response) {
   const { username, password } = req.body;
@@ -27,19 +28,21 @@ export default async function SiginIn(req: Request, res: Response) {
     if (!isPasswordVaild)
       return res.status(400).send("password or username invaild");
     if (!prvKey) return res.status(500).send("something went wrong!");
-    const token = jwt.sign(AdminData._id, prvKey, {
+
+    const token = jwt.sign({ id: AdminData?._id.toString() }, prvKey, {
       expiresIn: "10h",
     });
-
     res.cookie("token", token, {
-      secure: process.env.NODE_ENV !== "development",
+      secure: process.env.NODE_ENV === "production",
       httpOnly: true,
-      expires: dayjs().add(10, "h").toDate(),
+      expires: dayjs(Date.now()).add(10, "h").toDate(),
     });
 
     return res.json("Signed in");
   } catch (error) {
     if (error instanceof Error) {
+      winston.error(`error while user try sign-in msg:${error.message}`);
+      console.log(error);
       return res
         .status(500)
         .send("unkown error happen while sgin in try again");
