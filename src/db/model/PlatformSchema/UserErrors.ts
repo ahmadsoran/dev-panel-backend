052eae80ce1ddd56b2bb28f8e77896dd7fa936a5
ahmadsoran <ahmadsorann@gmail.com>
2023-01-24 00:13:53 +0300

    bug fix

---
+++ src/db/model/PlatformSchema/UserErrors.ts
@@ -0,0 +1,24 @@
+import { model, Schema } from "mongoose";
+import { UserErrors } from "../../../@types/User";
+
+const UserErrors = new Schema<UserErrors>(
+  {
+    cause: String,
+    informedBy: {
+      type: Schema.Types.ObjectId,
+      ref: "admin-account",
+    },
+    priority: {
+      type: String,
+      default: "normal",
+      enum: ["normal", "urgent", "critical"],
+    },
+    status: String,
+    times: Number,
+  },
+  { timestamps: true }
+);
+
+const ErrorsSchema = model("user-errors", UserErrors);
+
+export default ErrorsSchema;
