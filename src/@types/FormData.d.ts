8d6c68d8360d38a0cc347a8cac8ff49e89426ac7
ahmadsoran <ahmadsorann@gmail.com>
2023-01-24 20:17:08 +0300

    add data

--- src/@types/FormData.d.ts
+++ src/@types/FormData.d.ts
@@ -1,4 +1,10 @@
+import { UserErrors } from "./User";
+
 export type SiginInData = {
   username: string;
   password: string;
 };
+
+export interface AddErrorsData extends UserErrors {
+  DeviceUUID: string;
+}
