6fdcae6ed49eefb0b60aa1871389d0b1b86d8045
ahmadsoran <ahmadsorann@gmail.com>
2023-01-24 20:18:18 +0300

    add filters to get users

--- src/controller/Platform/GetUsers.ts
+++ src/controller/Platform/GetUsers.ts
@@ -6,28 +6,76 @@ type Queries = {
   page: string;
   row: string;
   search: string;
-  words: "asc" | "desc";
   date: "old" | "new";
-  errors: UserErrors["priority"];
-  os: "all" | "android" | "ios";
+  errorsPriority: UserErrors["priority"];
+  errorsType: UserErrors["status"];
+  searchBy: "model" | "UUID" | "brand";
 };
 
 export async function GetUsersOfPlatform(req: Request, res: Response) {
-  const { page, row, search, date, errors, os, words } = req.query as Queries;
+  const { page, row, search, date, errorsPriority, errorsType, searchBy } =
+    req.query as Queries;
 
   if (!req.platfomData._id) return res.status(400).send("invaild platform id");
   const resultsPerPage =
     parseInt(row as string) >= 1 ? parseInt(row as string) : 25;
   let pages = parseInt(page as string) >= 1 ? parseInt(page as string) - 1 : 0;
 
+  const isSearchFilterAvailable = search && searchBy ? true : false;
   try {
     const users = await UsersSchema.find({
       platform: req.platfomData._id,
-    });
+    })
+      .populate({
+        path: "Errors",
+        model: "user-errors",
+        select: "priority , status",
+        options: {
+          lean: true,
+        },
+      })
+      .sort(date === "new" ? "-createdAt" : "createdAt")
+      .lean()
+      .exec();
 
+    const FilterUsers = users
+      .filter((data) => {
+        if (isSearchFilterAvailable) {
+          if (searchBy === "model") return data.Device.model.includes(search);
+          else if (searchBy === "UUID")
+            return data.Device.UUID.includes(search);
+          else if (searchBy === "brand")
+            return data.Device.brand.includes(search);
+        } else return data;
+      })
+      .sort((a, b) => {
+        if (errorsPriority)
+          return (
+            b.Errors.filter((data) => data.priority === errorsPriority).length -
+            a.Errors.filter((data) => data.priority === errorsPriority).length
+          );
+        else
+          return (
+            a.Errors.filter((data) => data.priority === errorsPriority).length -
+            b.Errors.filter((data) => data.priority === errorsPriority).length
+          );
+      })
+      .sort((a, b) => {
+        if (errorsType)
+          return (
+            b.Errors.filter((data) => data.status === errorsType).length -
+            a.Errors.filter((data) => data.status === errorsType).length
+          );
+        else
+          return (
+            a.Errors.filter((data) => data.status === errorsType).length -
+            b.Errors.filter((data) => data.status === errorsType).length
+          );
+      })
+      .slice(pages * resultsPerPage, pages * resultsPerPage + resultsPerPage);
     return res.json({
       platform: req.platfomData,
-      users,
+      users: FilterUsers,
     });
   } catch (error) {
     if (error instanceof Error) {
