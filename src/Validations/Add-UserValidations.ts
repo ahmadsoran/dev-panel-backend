c77969b892735d0dc038c051a7fca71968678dac
ahmadsoran <ahmadsorann@gmail.com>
2023-01-26 16:52:26 +0300

    add validations

---
+++ src/Validations/Add-UserValidations.ts
@@ -0,0 +1,20 @@
+import Joi from "joi";
+import { User, UserDivice } from "../@types/User";
+
+const AddUserPlatformValidations = Joi.object<User>({
+  Device: Joi.object<UserDivice>({
+    brand: Joi.string().required(),
+    CPU: Joi.string().required(),
+    GPU: Joi.string().required(),
+    model: Joi.string().required(),
+    RAM: Joi.string().required(),
+    software_restrictions: Joi.string()
+      .valid("lock", "unloack" as UserDivice["software_restrictions"])
+      .default("lock")
+      .required(),
+    UUID: Joi.string().uuid().required(),
+    version: Joi.string().required(),
+  }).required(),
+});
+
+export default AddUserPlatformValidations;
